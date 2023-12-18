0e8006ce480fa78b393f773ca690e764b3067985
Joal1291 <jordan.albert@github.com>
2023-12-18 18:37:55 +0100

    update post method

--- src/main.py
+++ src/main.py
@@ -1,31 +1,17 @@
-from typing import Union
+from typing import Union, Optional
 
-from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi import FastAPI, HTTPException
+from fastapi import Path, Query
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
 
 app = FastAPI()
 
-inventory = {
-    1: {
-        "name":"milk",
-        "price": 132,
-        "brand": "regular"
-    },
-    2: {
-        "name":"playstation",
-        "price": 132,
-        "brand": "regular"
-    },
-    3: {
-        "name":"fromage",
-        "price": 132,
-        "brand": "regular"
-    },
-    4: {
-        "name":"xbox",
-        "price": 132,
-        "brand": "regular"
-    }
-}
+inventory = {}
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(description ="The ID of the item you'd like to view")):
@@ -36,10 +22,18 @@ def get_item(item_id: int = Path(description ="The ID of the item you'd like to 
 @app.get("/get-by-name")
 def get_item(name : str = Query(None, description="The name of the item you are looking for")):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     raise HTTPException(status_code=404, detail="Item not found in the inventory")
 
 @app.get("/all_item")
 def get_all_item():
     return inventory
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        raise HTTPException(status_code=405, detail="Item allready exist")
+    
+    inventory[item_id] = item
+    raise HTTPException(status_code=200, detail="Item created with succes")
